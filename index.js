0b999ce6ac231d517b6f93f322d9038426162c9f
kapil khurana <kapilkhurana89@gmail.com>
2022-05-25 16:28:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const fs = require("fs")
+
+app= express();
+
+
+fs.read('input.txt')
+
+
+// , function(err,data){
+//     if(err){
+//         console.log('Error:',err)
+//     }
+
+//     console.log('file opened sucessfully');
+// })
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
